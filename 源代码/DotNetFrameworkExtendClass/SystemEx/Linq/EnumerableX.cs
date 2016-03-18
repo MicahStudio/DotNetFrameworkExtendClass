746994c324bafd0655a43ac4d0abc0bee27c5fec
欧俊 <jun.ou@miaofangbao.cn>
2016-03-18 09:43:09 +0800

    去掉无效的引用

--- 源代码/DotNetFrameworkExtendClass/SystemEx/Linq/EnumerableX.cs
+++ 源代码/DotNetFrameworkExtendClass/SystemEx/Linq/EnumerableX.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DotNetFrameworkExtendClass
+﻿namespace DotNetFrameworkExtendClass
 {
     /// <summary>
     /// 提供一组用于查询实现 System.Collections.Generic.IEnumerable&lt;T&gt; 的对象的 static（在 Visual Basic 中为 Shared）方法。
